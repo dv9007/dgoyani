8a1b1108da8194596b9e3dd5d68fe0511554a589
dv9007 <goyanidarshak007uk@gmail.com>
2022-05-04 19:45:11 +0100

    classadded

---
+++ src/table4.java
@@ -0,0 +1,16 @@
+public class table4 {
+
+
+    public static void main (String [] args) {
+
+        int num = 2, i = 1;
+
+        while (i <= 10) {
+
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+
+            i++;
+
+        }
+    }
+}
